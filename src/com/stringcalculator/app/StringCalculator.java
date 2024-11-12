7c8a544e81671ac49ddb312a8d3c2b395d94f3e4
unknown <m.rudrapad@gmail.com>
2024-11-12 16:57:44 +0530

    code refactor

--- src/com/stringcalculator/app/StringCalculator.java
+++ src/com/stringcalculator/app/StringCalculator.java
@@ -1,6 +1,9 @@
 package com.stringcalculator.app;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 	
@@ -14,23 +17,11 @@ public class StringCalculator {
 			return parseSingleNumberInput(inputString);
 		}
 		
-		int sum = 0;
+		String[] numberStrings = checkForCustomDelimiterAndSplitTheString(inputString);
 		
-		String delimiter = ",|\\n";
-
-		if (inputString.startsWith("//")) {
-			int delimiterPatternEndIndex = inputString.indexOf("\n");
-			delimiter = Pattern.quote(inputString.substring(2, delimiterPatternEndIndex));
-			inputString = inputString.substring(delimiterPatternEndIndex + 1);
-		}
-
-		String[] nums = inputString.split(delimiter);
+		List<String> nonEmptyNumberStrings = checkForEmptyString(numberStrings);
 		
-		for (String str: nums) {
-			sum += Integer.parseInt(str);
-		}
-		
-		return sum;
+		return calculateSum(nonEmptyNumberStrings);
 	}
 	
 	private int parseSingleNumberInput(String inputString) {
@@ -40,6 +31,31 @@ public class StringCalculator {
 		}
 		
 		return Integer.parseInt(inputString);
+	}
+	
+	private List<String> checkForEmptyString(String[] nums) {
+		return Arrays.stream(nums) 
+				.filter(num -> !num.isEmpty())
+				.collect(Collectors.toList());
+	}
+	
+	private String[] checkForCustomDelimiterAndSplitTheString(String inputString) {
+		String delimiter = ",|\\n";
+		
+		if (inputString.startsWith("//")) {
+			int delimiterPatternEndIndex = inputString.indexOf("\n");
+			delimiter = Pattern.quote(inputString.substring(2, delimiterPatternEndIndex));
+			inputString = inputString.substring(delimiterPatternEndIndex + 1);
+		}
+		
+		String[] nums = inputString.split(delimiter);
+		return nums;
+	}
+	
+	private int calculateSum(List<String> nonEmptyNumberStrings) {
+		return nonEmptyNumberStrings.stream()
+				.mapToInt(Integer::parseInt)
+				.sum();
 	}
 	
 }

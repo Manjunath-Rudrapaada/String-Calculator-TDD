a05b5bedfa3724cbf52a2ae9061cf2e94a1e77f3
unknown <m.rudrapad@gmail.com>
2024-11-12 14:24:49 +0530

    condition to check for empty string and string containing only one non-integer character

--- src/com/stringcalculator/app/StringCalculator.java
+++ src/com/stringcalculator/app/StringCalculator.java
@@ -2,16 +2,34 @@ package com.stringcalculator.app;
 
 public class StringCalculator {
 	
-	public int add(String numbers) {
+	public int add(String inputString) {
+		
+		if (inputString.isEmpty()) {
+			return 0;
+		}
+		
+		if (inputString.length() == 1) {
+			return parseSingleNumberInput(inputString);
+		}
+		
 		int sum = 0;
 		
-		String[] arr = numbers.split(",");
+		String[] arr = inputString.split(",");
 		
 		for (String str: arr) {
 			sum += Integer.parseInt(str);
 		}
 		
 		return sum;
+	}
+	
+	private int parseSingleNumberInput(String inputString) {
+		if (!inputString.matches("\\d+")) { // regex to check if string contains number or a character
+			System.out.println("Invalid Input :" + inputString);
+			return -1;
+		}
+		
+		return Integer.parseInt(inputString);
 	}
 	
 }
